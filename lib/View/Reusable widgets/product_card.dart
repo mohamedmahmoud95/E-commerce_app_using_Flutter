312c3436ce6d1a45138faff6c88bd45223cd2dd5
Mohamed <m.raslan97@gmail.com>
2023-07-01 23:09:58 +0300

    edited main.dart

---
+++ lib/View/Reusable widgets/product_card.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatefulWidget {
+  const ProductCard({Key? key}) : super(key: key);
+
+  @override
+  State<ProductCard> createState() => _ProductCardState();
+}
+
+class _ProductCardState extends State<ProductCard> {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: (){
+
+      },
+
+      child: Card(
+        child: Container(
+
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/View/Screens/market_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MarketScreen extends StatefulWidget {
+  const MarketScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MarketScreen> createState() => _MarketScreenState();
+}
+
+class _MarketScreenState extends State<MarketScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Market"),
+      ),
+    );
+  }
+}
