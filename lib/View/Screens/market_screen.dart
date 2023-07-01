fd9da35fa02f602d14ff2fd078a094c4b8911c4f
Mohamed <m.raslan97@gmail.com>
2023-07-01 23:35:06 +0300

    added project colors

--- lib/View/Screens/market_screen.dart
+++ lib/View/Screens/market_screen.dart
@@ -1,3 +1,6 @@
+import 'package:e_commerce_app/View/Reusable%20widgets/product_card.dart';
+import 'package:e_commerce_app/View/Reusable%20widgets/rating_bar_indicator.dart';
+import 'package:e_commerce_app/View/UI%20constants/project_colors.dart';
 import 'package:flutter/material.dart';
 
 class MarketScreen extends StatefulWidget {
@@ -11,9 +14,13 @@ class _MarketScreenState extends State<MarketScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: mainWhite,
       appBar: AppBar(
         title: Text("Market"),
+        elevation: 0,
       ),
+
+      body: const ProductCard(),
     );
   }
 }
